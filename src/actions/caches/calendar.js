81d1cf064e8a7144042e4321d9824a922fc3bdd6
Gwenaelle Duchesne <gwenaelle.duchesne@gmail.com>
2023-06-28 17:54:38 +0200

    calendar

--- src/actions/caches/calendar.js
+++ src/actions/caches/calendar.js
@@ -1,10 +1,12 @@
-import { Text } from '@botonic/react'
+import { Text, Document } from '@botonic/react'
 import React from 'react'
+import calendar from '../../assets/calendar.png'
 
 export default class extends React.Component {
   render() {
     return (
       <>
+        <Document src={calendar} />
         <Text>This calendar is making me thirsty 🥵 </Text>
       </>
     )
