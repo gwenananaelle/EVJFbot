1591f469414c541202b860478fc998438abb78c9
Gwenaelle Duchesne <gwenaelle.duchesne@gmail.com>
2023-06-28 13:36:49 +0200

    changed hi text

--- src/actions/hi.js
+++ src/actions/hi.js
@@ -11,7 +11,8 @@ export default class extends React.Component {
       <>
       <Text>You have been invited by your friend Edvina to her bachelorette’s party. Which is weird since she was single the last time you saw her but whatever.</Text>
       <Text>I’m so happy to see you, come in, come in !</Text>
-      <Text>The apartment is decorated with balloons, there is music playing, cocktails and hors d'oeuvres on the table, and friends sitting around chatting. Would you like to listen to the music ? Or maybe a drink ?</Text>
+      <Text>The apartment is decorated with balloons, there is music playing, cocktails and hors d'oeuvres on the table, and friends sitting around chatting.</Text>
+      <Text>Sit down, get comfortable, enjoy the music...I made sex on the beach, would you like one ? When you're ready let me know and I'll tell you what it's all about 😁</Text>
       </>
     )
   }
