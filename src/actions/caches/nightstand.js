634e74da2b6d991baa742bce59f9a7ebd369a25d
Gwenaelle Duchesne <gwenaelle.duchesne@gmail.com>
2023-06-28 17:55:24 +0200

    changed name for asset

--- src/actions/caches/nightstand.js
+++ src/actions/caches/nightstand.js
@@ -1,12 +1,12 @@
 import { Text, Image, Document,} from '@botonic/react'
 import React from 'react'
-import clue from '../../assets/kamasutra.png'
+import kamasutra from '../../assets/kamasutra.png'
 
 export default class extends React.Component {
   render() {
     return (
       <>
-        <Document src={clue} />
+        <Document src={kamasutra} />
         {/* <Image src='https://media3.giphy.com/media/gtPaaCbkxpmWk/giphy.gif' /> */}
         <Text>I know some of those positions, but not all...I can't see the flying whale in there</Text>
       </>
