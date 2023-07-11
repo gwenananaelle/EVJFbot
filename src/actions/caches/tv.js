22e4e85aa2bf732cb10a02cedfb64233d5c59fe7
Gwenaelle Duchesne <gwenaelle.duchesne@gmail.com>
2023-07-11 17:06:09 +0200

    lips on tv

--- src/actions/caches/tv.js
+++ src/actions/caches/tv.js
@@ -1,10 +1,14 @@
-import { Text } from '@botonic/react'
+import { Text, Document } from '@botonic/react'
 import React from 'react'
+import lips from '../../assets/lips.png'
+import lips_alphabet from '../../assets/lips_alphabet.png'
 
 export default class extends React.Component {
   render() {
     return (
       <>
+        <Document src={lips} />
+        <Document src={lips_alphabet} />
         <Text>I can't stop looking at those lips, it's like they're trying to say something to me</Text>
       </>
     )
