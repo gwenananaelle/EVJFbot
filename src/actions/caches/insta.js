18e380a1ceb38ec68a17f0a2f110459f1d2a26a4
Gwenaelle Duchesne <gwenaelle.duchesne@gmail.com>
2023-07-11 16:15:09 +0200

    insta handle

---
+++ src/actions/caches/insta.js
@@ -0,0 +1,12 @@
+import { Text } from '@botonic/react'
+import React from 'react'
+
+export default class extends React.Component {
+  render() {
+    return (
+      <>
+        <Text>I know she has an insta but I forgot her handle..It's "Edvina_" with numbers after but which ones ?</Text>
+      </>
+    )
+  }
+}
