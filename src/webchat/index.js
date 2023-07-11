22ed466e1c271b95ace3817d759170a162101103
Gwenaelle Duchesne <gwenaelle.duchesne@gmail.com>
2023-07-11 19:16:38 +0200

    style : avatar and color

--- src/webchat/index.js
+++ src/webchat/index.js
@@ -1,4 +1,15 @@
+import Avatar from '../assets/avatar.png'
+
 export const webchat = {
     storage: sessionStorage,
     storageKey: 'bot-EVJF',
+    theme: {
+        brand: {
+            color: '#E0B6C8',
+            image: Avatar,
+        },
+        triggerButton: {
+            image: Avatar,
+          },
+    },
 }
