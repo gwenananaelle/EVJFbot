51c37de79b01d6cc61be1bea35a7069d2aa0dc73
Gwenaelle Duchesne <gwenaelle.duchesne@gmail.com>
2023-07-13 17:19:18 +0200

    fixed music

--- src/actions/music.js
+++ src/actions/music.js
@@ -1,16 +1,18 @@
-import { Text } from '@botonic/react'
+import { Text, Button } from '@botonic/react'
 import React from 'react'
 
 export default class extends React.Component {
-static async botonicInit({ input, session }) {
-    let answer = input.defaultFallback
-    return { answer, session}
-    }
+  static async botonicInit({ input, session }) {
+      let answer = input.defaultFallback
+      return { answer, session}
+      }
   render() {
-    return 
+    return (
       <>
         <Text>Those are my favorite songs right now, do you like it ? 🥰🕺 </Text>
         <Button url='https://deezer.page.link/8rP3TbaaX42hYeyv7'>Listen to the playlist</Button>
       </>
+    )
   }
 }
+
